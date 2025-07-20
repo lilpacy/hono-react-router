d53828adcba28f1fe3c914e3a15ce25d55420963
lilpacy <revivedtomorrow@gmail.com>
2025-07-20 23:10:15 +0900

    update configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: 'sqlite',
   driver: 'd1-http',
   dbCredentials: {
-    databaseId: '<your-database-id>',
+    databaseId: '67167978-02ea-430f-9a90-0d5a59c95e55',
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
